2bdd57b82f625863e38749c8d30936562416438c
hibohiboo <swdice66@gmail.com>
2019-11-09 10:25:49 +0900

    スマホ用のアラートを表示

---
+++ public/index.js
@@ -0,0 +1,15 @@
+$(function() {
+  $("img.lazy").lazyload();
+  const $j_screen = $(".rotage_alert");
+  $j_screen.on("click", e => {
+    $j_screen.remove();
+  });
+
+  $(window).bind("load orientationchange", function() {
+    if (Math.abs(window.orientation) === 90) {
+      $j_screen.remove();
+    } else {
+      $j_screen.show();
+    }
+  });
+});
