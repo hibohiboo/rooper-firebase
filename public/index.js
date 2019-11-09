af3e77678db74c184c0cdaf486713a8efb510cc8
hibohiboo <swdice66@gmail.com>
2019-11-09 17:58:18 +0900

    サービスワーカの登録

--- public/index.js
+++ public/index.js
@@ -12,4 +12,17 @@ $(function() {
       $j_screen.show();
     }
   });
+
+  window.addEventListener("load", function() {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(function(registration) {
+          console.log("serviceWorker registed.");
+        })
+        .catch(function(error) {
+          console.warn("serviceWorker error.", error);
+        });
+    }
+  });
 });
